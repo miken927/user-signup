f32343b0c7abf4d50cee50cce6e70f661c468a55
Mike Novak <miken927@hotmail.com>
2017-01-30 20:09:51 -0600

    pre validation form complete

--- main.py
+++ main.py
@@ -18,21 +18,21 @@ import webapp2
 
 # html boilerplate for the top of every page
 page_header = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>User Signup</title>
-    <style type="text/css">
-        .error {
-            color: red;
-        }
-    </style>
-</head>
-<body>
-    <h1>
-        <a href="/">User Signup</a>
-    </h1>
-"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>User Signup</title>
+        <style type="text/css">
+            .error {
+                color: red;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>
+            <a href="/">User Signup</a>
+        </h1>
+    """
 
 # html boilerplate for the bottom of every page
 page_footer = """
@@ -40,37 +40,47 @@ page_footer = """
 </html>
 """
 
+# initialize values
+username = ""
+email = ""
+username_error = ""
+password_error = ""
+email_error = ""
+
 signup_form = """
 <form method="post">
-    <p>Please complete the following form:</p>
     <table>
         <tr>
-            <td>Username:</td> <td><input type="text" name="username"/></td>
+            <td>Username:</td> <td><input type="text" name="username" value="{0}"/>{1}</td>
         </tr>
         <tr>
-            <td>Password:</td> <td><input type="password" name="password"/></td>
+            <td>Password:</td> <td><input type="password" name="password"/>{2}</td>
         </tr>
         <tr>
             <td>Re-enter password:</td> <td><input type="password" name="verify_password"/></td>
         <tr>
-            <td>E-mail:</td> <td><input type="E-mail" name="email"/></td>
+            <td>E-mail:</td> <td><input type="email" name="email" value="{3}"/>{4}</td>
         </tr>
     </table>
     <input type="submit" value="Sign Up!"/>
 </form>
-"""
-
+""".format(username, username_error, password_error, email, email_error)
 
 class welcome(webapp2.RequestHandler):
     def post(self):
+        self.request.get("username")
         self.response.write("Welcome " + username + "!")
 
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
 
+    def get(self):
         content = page_header + signup_form + page_footer
         self.response.write(content)
+
+    def post(self):
+        username = self.request.get("username")
+        self.response.write("Hello " + username)
 
 
 app = webapp2.WSGIApplication([
